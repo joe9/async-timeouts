73edfed2bfae25a535ee0e631520467bd0c7ac01
joe9 <joe9mail@gmail.com>
2017-07-10 14:48:53 -0700

    added function to async timeout in seconds

--- AsyncTimeouts.hs
+++ AsyncTimeouts.hs
@@ -2,7 +2,8 @@
 
 module AsyncTimeouts
   ( waitTimeout
-  , asyncTimeout_
+  , asyncTimeout
+  , asyncTimeoutInSeconds
   ) where
 
 -- got this from https://gist.github.com/robstewart57/4195051
@@ -11,6 +12,8 @@ import Control.Concurrent.Async (Async, race, wait, withAsync)
 import Control.Exception        (Exception, throw)
 import Control.Monad            (void)
 import Data.Typeable            (Typeable)
+
+import ThreadDelayInSeconds (threadDelayInSeconds)
 
 data ThreadTimeoutException =
   ThreadTimeoutException
@@ -21,7 +24,12 @@ instance Exception ThreadTimeoutException
 waitTimeout :: Async a -> Int -> IO (Either ThreadTimeoutException a)
 waitTimeout t i = race (threadDelay i >> throw ThreadTimeoutException) (wait t)
 
-asyncTimeout_ :: IO a -> Int -> IO ()
-asyncTimeout_ f i =
+asyncTimeout :: IO a -> Int -> IO ()
+asyncTimeout f i =
   withAsync f $ \a1 ->
     withAsync (threadDelay i) $ \a2 -> void $ race (wait a1) (wait a2)
+
+asyncTimeoutInSeconds :: IO a -> Int -> IO ()
+asyncTimeoutInSeconds f i =
+  withAsync f $ \a1 ->
+    withAsync (threadDelayInSeconds i) $ \a2 -> void $ race (wait a1) (wait a2)
